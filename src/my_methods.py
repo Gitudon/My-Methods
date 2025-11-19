5f9fc59e0d902853ac158286edc512843da9e433
Gitudon <oniku.udon.029@gmail.com>
2025-11-19 10:50:57 +0900

    関数追加

--- src/my_methods.py
+++ src/my_methods.py
@@ -1,2 +1,24 @@
 def sort_string(string: str) -> str:
     return ("").join(sorted(string))
+
+
+# string.count(char)で置き換え可能
+def count_char(string: str, char: str) -> int:
+    res = 0
+    for s in string:
+        if s == char:
+            res += 1
+    return res
+
+
+def add_chr_to_set(string: str, set_of_string: set) -> set:
+    for char in string:
+        set_of_string.add(char)
+    return set_of_string
+
+
+def count_loss(string: str, set_of_compared_string: set) -> int:
+    loss = 0
+    for char in string:
+        if char not in set_of_compared_string:
+            loss += 1
