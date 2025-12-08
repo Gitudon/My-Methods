2753af30f544e382779e523347b81f0e9803e9b8
Gitudon <oniku.udon.029@gmail.com>
2025-12-08 21:09:29 +0900

    三角形の面積

--- src/my_methods.py
+++ src/my_methods.py
@@ -45,3 +45,7 @@ def run_length_encoding(string: str) -> list:
             current = 1
     assyuku.append((focus, current))
     return assyuku
+
+
+def triangle_area(x1, y1, x2, y2, x3, y3):
+    return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
