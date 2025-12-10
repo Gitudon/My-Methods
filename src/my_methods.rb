7111fe2423fc33bec1b50e41c7e073fc3dfa65ad
Gitudon <oniku.udon.029@gmail.com>
2025-12-10 14:01:25 +0900

    ガンマ関数

--- src/my_methods.rb
+++ src/my_methods.rb
@@ -9,3 +9,14 @@ def ping_to_ip(ip_address)
     false
   end
 end
+
+
+def gamma_function(n)
+  if n <= 0
+    raise ArgumentError, "Input must be a positive number."
+  elsif n == 1
+    1
+  else
+    (n - 1) * gamma_function(n - 1)
+  end
+end
